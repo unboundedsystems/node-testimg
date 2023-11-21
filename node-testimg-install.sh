82219439ad6a413c9515f6296bf9d2310816b708
Mark Terrel <mark@adaptable.io>
2023-11-20 19:42:14 -0700

    feat: Add buildx plugin to docker

--- node-testimg-install.sh
+++ node-testimg-install.sh
@@ -34,7 +34,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 add-apt-repository \
     "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 apt-get update
-apt-get install -y --no-install-recommends docker-ce
+apt-get install -y --no-install-recommends docker-ce docker-buildx-plugin
 
 # yarn
 if [[ ${NODE_VER} =~ ^10 ]]; then
